e4badd6860572aea146c8248e19210e01fab2466
Samira Sobhani <ss121280@yahoo.com>
2020-06-04 14:10:49 -0500

    first commit

---
+++ src/main/java/com/StateCapitals/StateCapitals/StateCapitalsApplication.java
@@ -0,0 +1,13 @@
+package com.StateCapitals.StateCapitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateCapitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateCapitalsApplication.class, args);
+	}
+
+}
