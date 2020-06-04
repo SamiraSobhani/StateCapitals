ea19cec161a8dfbeda7f6d5a67ee7c63a064ab1f
Samira Sobhani <ss121280@yahoo.com>
2020-06-04 14:50:17 -0500

    print the states

--- src/main/java/com/StateCapitals/StateCapitals/StateCapitalsApplication.java
+++ src/main/java/com/StateCapitals/StateCapitals/StateCapitalsApplication.java
@@ -3,11 +3,32 @@ package com.StateCapitals.StateCapitals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 @SpringBootApplication
 public class StateCapitalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StateCapitalsApplication.class, args);
+
+		HashMap<String,String> StateCapitals = new HashMap<>();
+
+		StateCapitals.put("Alabama","Montgomery");
+		StateCapitals.put("Alaska", "Juneau");
+		StateCapitals.put("Arizona" , "Phoenix");
+		StateCapitals.put("Arkansas", "Little Rock");
+		System.out.println(StateCapitals.size());
+
+		Set<String> states = StateCapitals.keySet();
+		for (String currentState : states){
+			System.out.println(currentState);
+		}
 	}
+
+
+
 
 }
