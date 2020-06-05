0e4011ea882df7df07cabb3a899cd98b63570fc7
Samira Sobhani <ss121280@yahoo.com>
2020-06-05 11:15:02 -0500

    final project

--- src/main/java/com/StateCapitals/StateCapitals/StateCapitalsApplication.java
+++ src/main/java/com/StateCapitals/StateCapitals/StateCapitalsApplication.java
@@ -14,21 +14,39 @@ public class StateCapitalsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StateCapitalsApplication.class, args);
 
-		HashMap<String,String> StateCapitals = new HashMap<>();
+		HashMap<String, String> StateCapitals = new HashMap<>();
 
-		StateCapitals.put("Alabama","Montgomery");
+		StateCapitals.put("Alabama", "Montgomery");
 		StateCapitals.put("Alaska", "Juneau");
-		StateCapitals.put("Arizona" , "Phoenix");
+		StateCapitals.put("Arizona", "Phoenix");
 		StateCapitals.put("Arkansas", "Little Rock");
 		System.out.println(StateCapitals.size());
 
 		Set<String> states = StateCapitals.keySet();
-		for (String currentState : states){
+
+		System.out.println("STATES:");
+		System.out.println("*********");
+		for (String currentState : states) {
 			System.out.println(currentState);
+
 		}
+
+		System.out.println("CAPITALS:");
+		System.out.println("**********");
+		for (String currentState : states) {
+			String capitals = StateCapitals.get(currentState);
+			System.out.println(capitals);
+
+		}
+
+		System.out.println("STATE/CAPITAL PAIRS");
+		System.out.println("**************");
+		for (String currentState : states) {
+			String capitals = StateCapitals.get(currentState);
+			System.out.println(currentState + " - " + capitals);
+		}
+
+
 	}
-
-
-
 
 }
